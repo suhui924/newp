cc04e43c69dd864edcfadb69cb1b71faebd265b3
suhui <suhui924@163.com>
2019-10-29 15:39:42 +0800

    对对对2

--- src/main/java/com/test/SpringbootApplication.java
+++ src/main/java/com/test/SpringbootApplication.java
@@ -2,10 +2,18 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String test() {
+        return "test Jekines";
     }
 }
